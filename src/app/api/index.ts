4d9d256392138ecb1356f8096628101002e043b0
Ramon Saviato <ramonsaviato@hotmail.com>
2024-07-05 22:05:25 -0300

    fix: change to https from http

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -4,7 +4,7 @@ import { fetchData } from '../http';
 import { delay, parseSearchParameters } from '../utils';
 
 const API_KEY = '2b16a002010d4dbb84113453242606';
-const BASE_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&aqi=no&alerts=no`;
+const BASE_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&aqi=no&alerts=no`;
 
 export async function getWeatherData(query: string, params?: SearchParams) {
   const parsedParameters = params ? `&${parseSearchParameters(params)}` : '';
